9ba7d4717c5b94783a9f8c8e60c0836fffaaec5f
SATTU3108 <127048911+SATTU3108@users.noreply.github.com>
2023-06-29 15:36:13 +0530

    Add files via upload

---
+++ Components/index.js
@@ -0,0 +1,7 @@
+import NavBar from "./NavBar/NavBar";
+import Filter from "./Filter/Filter";
+import Model from "./Model/Model";
+import UserCard from "./UserCard/UserCard";
+import Friend from "./Friend/Friend";
+
+export { NavBar, Filter, Model, UserCard, Friend };
