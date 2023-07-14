f8afcd3184a320dcb2b011bd4c721a5c9ae9d0cc
SATTU3108 <127048911+SATTU3108@users.noreply.github.com>
2023-07-14 23:06:26 +0530

    Update Friend.jsx

--- Components/Friend/Friend.jsx
+++ Components/Friend/Friend.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 //INTERNAL IMPORT
 import Style from "./Friend.module.css";
 import images from "../../images";
+import Card from "./Card/Card";
 import Chat from "./Chat/Chat";
 import { ChatAppContect } from "../../Context/ChatAppContext";
 
