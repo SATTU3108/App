817d165bbf24f272f1aa28029e144ca50380d08f
SATTU3108 <127048911+SATTU3108@users.noreply.github.com>
2023-07-25 15:47:10 +0530

    Update Card.jsx

--- Components/Friend/Card/Card.jsx
+++ Components/Friend/Card/Card.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-//INTERNAL IMPORT
 import Style from "./Card.module.css";
 import images from "../../../images";
 
@@ -32,6 +31,7 @@ const Card = ({ readMessage, el, i, readUser }) => {
           <div className={Style.Card_box_right}>
             <div className={Style.Card_box_right_middle}>
               <h4>{el.name}</h4>
+              <small>{el.pubkey.slice(21)}..</small>
             </div>
             <div className={Style.Card_box_right_end}>
               <small>{i + 1}</small>
